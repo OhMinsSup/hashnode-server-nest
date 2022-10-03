73d82ff8b861af497cad414c5929d4b0d9c95315
OhMinSeop <mins5190@gmail.com>
2022-10-03 11:14:09 +0900

    add: 게시물 등록 API 수정

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,10 +3,17 @@ import { Injectable } from '@nestjs/common';
 // service
 import { PrismaService } from '../modules/database/prisma.service';
 
-// types
-import { type AuthUserSchema } from '../libs/get-user.decorator';
-import { CreateRequestDto } from './dto/create.request.dto';
+// utils
+import { isEmpty } from '../libs/assertion';
+import { escapeForUrl } from '../libs/utils';
+
+// constants
 import { EXCEPTION_CODE } from 'src/constants/exception.code';
+
+// types
+import { CreateRequestDto } from './dto/create.request.dto';
+import type { Tag } from '@prisma/client';
+import type { AuthUserSchema } from '../libs/get-user.decorator';
 
 @Injectable()
 export class PostsService {
@@ -18,30 +25,69 @@ export class PostsService {
    * @param {CreateRequestDto} input
    */
   async create(user: AuthUserSchema, input: CreateRequestDto) {
-    const post = await this.prisma.post.create({
-      data: {
-        userId: user.id,
-        title: input.title,
-        subTitle: input.subTitle ?? null,
-        content: input.content,
-        description: input.description,
-        thumbnail: input.thumbnail ?? null,
-        tags: {
-          connectOrCreate: (input.tags ?? []).map((tag) => ({
-            where: { name: tag },
-            create: { name: tag },
-          })),
-        },
-      },
-    });
+    return this.prisma.$transaction(async (tx) => {
+      let createdTags: Tag[] = [];
+      // 태크 체크
+      if (!isEmpty(input.tags) && input.tags) {
+        const tags = await Promise.all(
+          input.tags.map(async (tag) => {
+            const name = escapeForUrl(tag);
+            const tagData = await tx.tag.findFirst({
+              where: {
+                name,
+              },
+            });
+            if (!tagData) {
+              return tx.tag.create({
+                data: {
+                  name,
+                },
+              });
+            }
+            return tagData;
+          }),
+        );
+        createdTags = tags;
+      }
 
-    return {
-      resultCode: EXCEPTION_CODE.OK,
-      message: null,
-      error: null,
-      result: {
-        dataId: post.id,
-      },
-    };
+      const post = await tx.post.create({
+        data: {
+          userId: user.id,
+          title: input.title,
+          subTitle: input.subTitle ?? null,
+          content: input.content,
+          description: input.description,
+          thumbnail: input.thumbnail ?? null,
+        },
+      });
+
+      await Promise.all(
+        createdTags.map((tag) =>
+          tx.postsTags.create({
+            data: {
+              post: {
+                connect: {
+                  id: post.id,
+                },
+              },
+              tag: {
+                connect: {
+                  id: tag.id,
+                },
+              },
+            },
+          }),
+        ),
+      );
+
+      return {
+        resultCode: EXCEPTION_CODE.OK,
+        message: null,
+        error: null,
+        result: {
+          dataId: post.id,
+        },
+      };
+    });
   }
 }
